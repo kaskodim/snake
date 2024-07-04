c3bef9ad67e641e54f0096b548245d1e7e287681
Дмитрий Гулевич <kaskodim@gmail.com>
2024-07-04 22:05:58 +0300

    правки после ревью

--- functions/startGame.js
+++ functions/startGame.js
@@ -2,9 +2,9 @@ import { main } from "./main.js";
 
 export function startGame() {
 
-    const difficultyLevel = document.querySelector('input[name="levelname"]:checked')
+    const difficultyLevel = document.querySelector('input[name="levelName"]:checked')
     const menuContainer = document.getElementById('menuContainer');
-    menuContainer.remove('menuContainer');
+    menuContainer.remove();
     main();
-    console.log(difficultyLevel.value)
+    console.log(difficultyLevel.value);
 }
